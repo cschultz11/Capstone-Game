75ff12cf89c6db12f84283ca09f25b56909b0f87
cschultz11 <91289400+cschultz11@users.noreply.github.com>
2023-02-19 12:27:16 -0500

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -10,8 +10,6 @@ class Spritesheet:
     def get_sprite(self, x, y, width, height):
         sprite = pygame.Surface([width, height], pygame.SRCALPHA)
         sprite.blit(self.sheet, (0,0), (x, y, width, height))
-        #sprite.set_colorkey(BLACK)
-        #self.image.set_colorkey(BLACK)
         return sprite
 
 class Player(pygame.sprite.Sprite):
@@ -116,6 +114,24 @@ class BottomFence(pygame.sprite.Sprite):
         self.rect.x = self.x
         self.rect.y = self.y
         
+class BottomFenceTopper(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = BLOCK_LAYER
+        self.groups = self.game.all_sprites, self.game.blocks
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        self.width = TILESIZE
+        self.height = TILESIZE
+        
+        self.image = self.game.terrain_spritesheet.get_sprite(64,0,self.width,self.height)
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
 class Ground(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
         self.game = game
@@ -128,10 +144,7 @@ class Ground(pygame.sprite.Sprite):
         self.width = TILESIZE
         self.height = TILESIZE
         
-        #self.image = pygame.Surface([self.width, self.height])
-        #self.image.fill(RED)
-        
-        self.image = self.game.terrain_spritesheet.get_sprite(64, 0, self.width, self.height)
+        self.image = self.game.terrain_spritesheet.get_sprite(96, 0, self.width, self.height)
         
         self.rect = self.image.get_rect()
         self.rect.x = self.x
